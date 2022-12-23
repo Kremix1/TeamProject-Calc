141b02404463f8a4a1851181b6e81936f9c18a91
Kremix1 <suttles123654@gmail.com>
2022-12-23 21:40:42 +0500

    Реализованы подсказки на каждую итерацию
    1)Подсказки на каждую итерацию
    2)Условный рендер итеративной таблицы

--- src/Components/Calculate.js
+++ src/Components/Calculate.js
@@ -8,6 +8,23 @@ import './Prompt.scss';
 import {TablePerson} from './TablePerson';
 import { RenderTable } from "./RenderTable";
 
+const calculatePrompt = (prompt) => {
+    switch(prompt){
+        case "iteration":
+            document.getElementById('iteration').classList.toggle('show-prompt')
+        case "allIteration":
+            document.getElementById('allIteration').classList.toggle('show-prompt')
+            break;
+        case "coefficient":
+            document.getElementById('coefficient').classList.toggle('show-prompt')
+            break;
+        case "comission":
+            document.getElementById('comission').classList.toggle('show-prompt')
+            break;
+    }
+}
+export {calculatePrompt}
+
 export default function Calculate(props){
     const [data, setData] = useState(props.dataInCalculate);
     //ToDo: Сделать динамическое создание столбцов
@@ -16,7 +33,6 @@ export default function Calculate(props){
             <div className="content">
                 <div className="content__title">
                     <div id="title" className="title__text">Калькулятор итоговой оценки</div>
-                    <Question isCalculate={true}/>
                     <div id="prompt" className='content__prompt prompt-first'>
                         <div className='prompt__arrow'></div>
                         <div className='prompt__body'>Это калькулятор итоговой оценки, в него можно внести предпологаемые вводные данные оценок по Итерациям, Коэффициент участия, Сводную оценку комиссии и посмотреть, что будет в итоге</div>
@@ -28,10 +44,34 @@ export default function Calculate(props){
                             <thead>
                                 <tr>
                                     <th></th>
-                                    <th><div className="table__title-wrapper"><div className="table__title">По всем итерациям</div><div className="question"></div></div></th>
-                                    <th><div className="table__title-wrapper"><div className="table__title">Коэффициент участия</div><div className="question"></div></div></th>
-                                    <th><div className="table__title-wrapper"><div className="table__title">Оценка экспертной комиссии</div><div className="question"></div></div></th>
-                                    <th><div className="table__title-wrapper"><div className="table__title">Итог</div><div className="question"></div></div></th>
+                                    <th><div className="table__title-wrapper"><div className="table__title">По всем итерациям</div>
+                                            <div className="question" onClick={() => calculatePrompt("allIteration")}></div>
+                                            <div id="allIteration" className="table-prompt">
+                                                <div className="table-prompt__content">
+                                                    <div className="table-prompt__text">Оценка по всем итерациям складывается по общему арифметическому из всех оценок каждой итерации</div>
+                                                    <div className="table-prompt__button" onClick={() => calculatePrompt("allIteration")}>Ок</div>
+                                                </div>
+                                            </div>
+                                        </div></th>
+                                    <th><div className="table__title-wrapper"><div className="table__title">Коэффициент участия</div>
+                                            <div className="question" onClick={() => calculatePrompt("coefficient")}></div>
+                                            <div id="coefficient" className="table-prompt">
+                                                <div className="table-prompt__content">
+                                                    <div className="table-prompt__text">Коэффициент участия - коэффициент, отражающий степень участия студента в достижении результатов итерации проекта</div>
+                                                    <div className="table-prompt__button" onClick={() => calculatePrompt("coefficient")}>Ок</div>
+                                                </div>
+                                            </div>
+                                        </div></th>
+                                    <th><div className="table__title-wrapper"><div className="table__title">Оценка экспертной комиссии</div>
+                                            <div className="question" onClick={() => calculatePrompt("comission")}></div>
+                                            <div id="comission" className="table-prompt">
+                                                <div className="table-prompt__content">
+                                                    <div className="table-prompt__text">Сводная оценка экспертной комиссии - это итоговый балл от экспертной комиссии, полученный студентами проектной команды, по результатам очной защиты проекта</div>
+                                                    <div className="table-prompt__button" onClick={() => calculatePrompt("comission")}>Ок</div>
+                                                </div>
+                                            </div>
+                                        </div></th>
+                                    <th><div className="table__title-wrapper"><div className="table__title">Итог</div></div></th>
                                 </tr>
                             </thead>
                             <tbody>
